e3bf562160f11fb21ef81997251c4bd32ed5f9f9
maarxx <chris.lewark@gmail.com>
2017-04-09 13:18:59 -0400

    Implements Issue #8

--- BulkDesignator/MainTab_BulkDesignator.cs
+++ BulkDesignator/MainTab_BulkDesignator.cs
@@ -32,12 +32,160 @@ namespace BulkDesignator
         public override MainTabWindowAnchor Anchor =>
             MainTabWindowAnchor.Right;
 
+        public struct SurgeryOption
+        {
+            public string text;
+            public RecipeDef recipe;
+            public BodyPartRecord part;
+
+            public bool equals(SurgeryOption so2)
+            {
+                return this.text == so2.text && this.recipe == so2.recipe && this.part == so2.part;
+            }
+        }
+
+        public string generateSurgeryText(Pawn pawn, RecipeDef recipe, BodyPartRecord part)
+        {
+            string text = recipe.Worker.GetLabelWhenUsedOn(pawn, part);
+            if (part != null && !recipe.hideBodyPartNames)
+            {
+                text = text + " (" + part.def.label + ")";
+            }
+            return text;
+        }
+
+        public bool doesPawnAlreadyHaveSurgery(Pawn pawn, SurgeryOption surgery)
+        {
+            foreach (Bill b in pawn.BillStack.Bills)
+            {
+                if (b is Bill_Medical)
+                {
+                    Bill_Medical existing = (Bill_Medical)b;
+                    if (existing.recipe == surgery.recipe)
+                    {
+                        if (existing.Part == surgery.part)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool canPawnGetSurgery(Pawn pawn, SurgeryOption surgery)
+        {
+            foreach (RecipeDef current in pawn.def.AllRecipes)
+            {
+                if (current.AvailableNow)
+                {
+                    IEnumerable<ThingDef> enumerable = current.PotentiallyMissingIngredients(null, pawn.Map);
+                    if (!enumerable.Any((ThingDef x) => x.isBodyPartOrImplant))
+                    {
+                        if (!enumerable.Any((ThingDef x) => x.IsDrug))
+                        {
+                            if (current.targetsBodyPart)
+                            {
+                                foreach (BodyPartRecord current2 in current.Worker.GetPartsToApplyOn(pawn, current))
+                                {
+                                    //list.Add(HealthCardUtility.GenerateSurgeryOption(pawn, pawn, current, enumerable, current2));
+                                    SurgeryOption temp = new SurgeryOption();
+                                    temp.text = generateSurgeryText(pawn, current, current2);
+                                    temp.recipe = current;
+                                    temp.part = current2;
+                                    if (surgery.equals(temp))
+                                    {
+                                        return true;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                //list.Add(HealthCardUtility.GenerateSurgeryOption(pawn, pawn, current, enumerable, null));
+                                SurgeryOption temp = new SurgeryOption();
+                                temp.text = generateSurgeryText(pawn, current, null);
+                                temp.recipe = current;
+                                temp.part = null;
+                                if (surgery.equals(temp))
+                                {
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public List<SurgeryOption> getAllPossibleSurgeries(List<Pawn> pawns)
+        {
+            HashSet<SurgeryOption> set = new HashSet<SurgeryOption>();
+            foreach (Pawn pawn in pawns)
+            {
+                foreach (RecipeDef current in pawn.def.AllRecipes)
+                {
+                    if (current.AvailableNow)
+                    {
+                        IEnumerable<ThingDef> enumerable = current.PotentiallyMissingIngredients(null, pawn.Map);
+                        if (!enumerable.Any((ThingDef x) => x.isBodyPartOrImplant))
+                        {
+                            if (!enumerable.Any((ThingDef x) => x.IsDrug))
+                            {
+                                if (current.targetsBodyPart)
+                                {
+                                    foreach (BodyPartRecord current2 in current.Worker.GetPartsToApplyOn(pawn, current))
+                                    {
+                                        //list.Add(HealthCardUtility.GenerateSurgeryOption(pawn, pawn, current, enumerable, current2));
+                                        SurgeryOption temp = new SurgeryOption();
+                                        temp.text = generateSurgeryText(pawn, current, current2);
+                                        temp.recipe = current;
+                                        temp.part = current2;
+                                        if (!doesPawnAlreadyHaveSurgery(pawn, temp))
+                                        {
+                                            set.Add(temp);
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    //list.Add(HealthCardUtility.GenerateSurgeryOption(pawn, pawn, current, enumerable, null));
+                                    SurgeryOption temp = new SurgeryOption();
+                                    temp.text = generateSurgeryText(pawn, current, null);
+                                    temp.recipe = current;
+                                    temp.part = null;
+                                    if (!doesPawnAlreadyHaveSurgery(pawn, temp))
+                                    {
+                                        set.Add(temp);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return set.ToList();
+        }
+
+        public void addSurgeryIfNotAlready(List<Pawn> pawns, SurgeryOption surgery)
+        {
+            foreach (Pawn pawn in pawns)
+            {
+                if (canPawnGetSurgery(pawn, surgery) && !doesPawnAlreadyHaveSurgery(pawn, surgery))
+                {
+                    Bill_Medical bm = new Bill_Medical(surgery.recipe);
+                    pawn.BillStack.AddBill(bm);
+                    bm.Part = surgery.part;
+                }
+            }
+        }
+
         public override void DoWindowContents(Rect canvas)
         {
             base.DoWindowContents(canvas);
 
             Text.Font = GameFont.Small;
-            for (int i = 0; i <= 2; i++)
+            for (int i = 0; i <= 3; i++)
             {
                 Rect nextButton = new Rect(canvas);
                 nextButton.y = i * (BUTTON_HEIGHT + BUTTON_SPACE);
@@ -87,83 +235,43 @@ namespace BulkDesignator
                         buttonLabel = "Operate All Mechanoids";
                         if (Widgets.ButtonText(nextButton, buttonLabel))
                         {
+                            List<Pawn> mechanoids = new List<Pawn>();
                             foreach (Pawn p in Find.VisibleMap.mapPawns.AllPawnsSpawned)
                             {
                                 if (p.ToString().Contains("Mechanoid") && p.Downed)
                                 {
-                                    foreach (RecipeDef recipe in p.def.AllRecipes)
-                                    {
-                                        if (recipe.AvailableNow)
-                                        {
-                                            IEnumerable<ThingDef> enumerable = recipe.PotentiallyMissingIngredients(null, p.Map);
-                                            if (!enumerable.Any((ThingDef x) => x.isBodyPartOrImplant))
-                                            {
-                                                if (!enumerable.Any((ThingDef x) => x.IsDrug))
-                                                {
-                                                    if (recipe.targetsBodyPart)
-                                                    {
-                                                        foreach (BodyPartRecord bodyPart in recipe.Worker.GetPartsToApplyOn(p, recipe))
-                                                        {
-                                                            bool alreadyExists = false;
-                                                            foreach (Bill b in p.BillStack.Bills)
-                                                            {
-                                                                if (b is Bill_Medical)
-                                                                {
-                                                                    Bill_Medical existing = (Bill_Medical)b;
-                                                                    if (existing.recipe.label == recipe.label)
-                                                                    {
-                                                                        if (existing.Part == bodyPart)
-                                                                        {
-                                                                            alreadyExists = true;
-                                                                        }
-                                                                    }
-                                                                }
-                                                            }
-                                                            if (!alreadyExists)
-                                                            {
-                                                                Bill_Medical bm = new Bill_Medical(recipe);
-                                                                p.BillStack.AddBill(bm);
-                                                                bm.Part = bodyPart;
-                                                                p.BillStack.Reorder(bm, 0);
-                                                            }
-                                                        }
-                                                    }
-                                                    else
-                                                    {
-                                                        bool alreadyExists = false;
-                                                        foreach (Bill b in p.BillStack.Bills)
-                                                        {
-                                                            if (b is Bill_Medical)
-                                                            {
-                                                                Bill_Medical existing = (Bill_Medical)b;
-                                                                if (existing.recipe.label == recipe.label)
-                                                                {
-                                                                    alreadyExists = true;
-                                                                }
-                                                            }
-                                                        }
-                                                        if (!alreadyExists)
-                                                        {
-                                                            Bill_Medical bm = new Bill_Medical(recipe);
-                                                            p.BillStack.AddBill(bm);
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
+                                    mechanoids.Add(p);
                                 }
                             }
+                            List<SurgeryOption> available = getAllPossibleSurgeries(mechanoids);
+                            List<FloatMenuOption> menuAvailable = new List<FloatMenuOption>();
+                            foreach (SurgeryOption so in available)
+                            {
+                                menuAvailable.Add(new FloatMenuOption(so.text, delegate { addSurgeryIfNotAlready(mechanoids, so); }));
+                            }
+                            Find.WindowStack.Add(new FloatMenu(menuAvailable));
                         }
                         break;
                     case 3:
-                        buttonLabel = "Current Enemy Shot Begun is: ";
-                        /* TODO: Implement
+                        buttonLabel = "Bulk Operate Humanoids";
                         if (Widgets.ButtonText(nextButton, buttonLabel))
                         {
-                            component.enemyShotBegun = !curEnemyShotBegun;
+                            List<Pawn> pawns = new List<Pawn>();
+                            foreach (object obj in Find.Selector.SelectedObjects)
+                            {
+                                if (obj is Pawn)
+                                {
+                                    pawns.Add((Pawn)obj);
+                                }
+                            }
+                            List<SurgeryOption> available = getAllPossibleSurgeries(pawns);
+                            List<FloatMenuOption> menuAvailable = new List<FloatMenuOption>();
+                            foreach (SurgeryOption so in available)
+                            {
+                                menuAvailable.Add(new FloatMenuOption(so.text, delegate { addSurgeryIfNotAlready(pawns, so); }));
+                            }
+                            Find.WindowStack.Add(new FloatMenu(menuAvailable));
                         }
-                        */
                         break;
                 }
             }
